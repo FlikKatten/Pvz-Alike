0b14295d23385de931c5a446cbaf3cf35453966d
FlikKatten <FlikKatten@users.noreply.github.com>
2020-10-09 12:47:44 -0300

    v01

---
+++ Assets/Scripts/Cards.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards : MonoBehaviour
+{
+
+    void OnMouseDown()
+    {
+        if(gameObject.tag == "CardTower01")
+        {
+            if(InterfaceController.moneyCount <= 25)
+            {
+                InterfaceController.moneyCount = InterfaceController.moneyCount - 25;
+            }
+        }
+        else if(gameObject.tag == "CardTower02")
+        {
+            if (InterfaceController.moneyCount <= 50)
+            {
+                InterfaceController.moneyCount = InterfaceController.moneyCount - 50;
+            }
+        }
+        else if(gameObject.tag == "CardTower03")
+        {
+            if (InterfaceController.moneyCount <= 75)
+            {
+                InterfaceController.moneyCount = InterfaceController.moneyCount - 75;
+            }
+        }
+        else if(gameObject.tag == "CardTower04")
+        {
+            if (InterfaceController.moneyCount <= 100)
+            {
+                InterfaceController.moneyCount = InterfaceController.moneyCount - 100;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceController : MonoBehaviour
+{
+    public Text scoreText, moneyText;
+    public static int moneyCount, scoreCount;
+
+    void Start()
+    {
+        scoreCount = 0;
+    }
+
+    void Update()
+    {
+        scoreText.text = scoreCount.ToString();
+        moneyText.text = moneyCount.ToString();
+    }
+}
